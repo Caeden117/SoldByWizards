c9eea052252d8247c07161ad89afba888dace025
Caeden117 <caeden.s@outlook.com>
2024-04-13 00:31:55 -0700

    Glyph and glyph hashing system

---
+++ Assets/Runtime/Glyphs/GlyphAggregator.cs
@@ -0,0 +1,38 @@
+using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace SoldByWizards.Glyphs
+{
+    public class GlyphAggregator : MonoBehaviour
+    {
+        [SerializeField] private GlyphController[] _glyphControllers;
+
+        [PublicAPI]
+        public bool AllGlyphsValid()
+        {
+            for (var i = 0; i < _glyphControllers.Length; i++)
+            {
+                if (!_glyphControllers[i].IsValidGlyph) return false;
+            }
+
+            return true;
+        }
+
+        [PublicAPI]
+        public int GetAggregateHash()
+        {
+            var hash = 0;
+
+            unchecked
+            {
+                for (var i = 0; i < _glyphControllers.Length; i++)
+                {
+                    hash |= _glyphControllers[i].GetGlyphHash() << (i * 8);
+                }
+            }
+
+            return hash;
+        }
+    }
+}
