293609074d3723866e6cc3c702ba386492305a66
Caeden117 <caeden.s@outlook.com>
2024-04-13 10:30:01 -0700

    - Backrooms map with baked lighting - Map Descriptor and basic item spawn

---
+++ Assets/Runtime/Maps/MapDescriptor.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SoldByWizards.Glyphs;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SoldByWizards.Maps
+{
+    public class MapDescriptor : MonoBehaviour
+    {
+        private static readonly int _randomHue = Shader.PropertyToID("_RandomHue");
+
+        [field: SerializeField]
+        public string Name { get; private set; }
+
+        [SerializeField] private MapObjectSpawnPoint[] _objectSpawnPoints;
+        [SerializeField] private int _guaranteedSpawns = 3;
+
+        private void Start()
+        {
+            // ahahahahahaha
+            var glyphAggregate = FindObjectOfType<GlyphAggregator>();
+
+            if (glyphAggregate == null) return;
+
+            // Init seed based on glyph aggregate
+            Random.InitState(glyphAggregate.GetAggregateHash());
+
+            // Random hue for map shaders
+            Shader.SetGlobalFloat(_randomHue, Random.Range(0, 360f));
+
+            // Early return if we have no items to spawn
+            if (_objectSpawnPoints.Length == 0) return;
+
+            // Ensure X amount of items always spawn
+            for (var i = 0; i < _guaranteedSpawns; i++)
+            {
+                var randomIdx = Random.Range(0, _objectSpawnPoints.Length);
+                _objectSpawnPoints[randomIdx].Spawn();
+            }
+
+            // Any other items will use the random chance spawn system
+            for (var i = 0; i < _objectSpawnPoints.Length; i++)
+            {
+                _objectSpawnPoints[i].RandomSpawn();
+            }
+        }
+    }
+}
