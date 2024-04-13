efc45139fccbe0ddb395aaa3914784da1441ab58
legoandmars <legoandmars@gmail.com>
2024-04-13 13:34:20 -0700

    item spam typing start

--- Assets/Runtime/Computers/ComputerItemEntryController.cs
+++ Assets/Runtime/Computers/ComputerItemEntryController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using SoldByWizards.Items;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SoldByWizards.Computers
 {
@@ -8,5 +9,34 @@ namespace SoldByWizards.Computers
     {
         // TODO: Hook up dynamically
         [SerializeField] public List<ItemSO> CurrentItems = new();
+        [SerializeField] private ItemListing _itemListingTemplate;
+        [SerializeField] private RectTransform _itemListingContainer;
+
+        private List<ItemListing> _spawnedItemListings = new();
+
+        // call when coming back from a teleport
+        public void CreateListings()
+        {
+            foreach (var listing in _spawnedItemListings)
+            {
+                listing.gameObject.SetActive(false);
+                Destroy(listing);
+            }
+            _spawnedItemListings.Clear();
+
+            foreach (var item in CurrentItems)
+            {
+                // create item
+                var itemListing = Instantiate(_itemListingTemplate, _itemListingContainer);
+                _spawnedItemListings.Add(itemListing);
+                itemListing.Clear();
+                itemListing.SetItem(item);
+            }
+        }
+
+        private void Start()
+        {
+            CreateListings();
+        }
     }
 }
--- Assets/Runtime/UI/ItemListing.cs
+++ Assets/Runtime/UI/ItemListing.cs
@@ -7,19 +7,45 @@ namespace SoldByWizards
 {
     public class ItemListing : MonoBehaviour
     {
+        public const string SellerName = "Sold by: Wizards";
+
         [SerializeField] private TextMeshProUGUI? _titleText;
         [SerializeField] private TextMeshProUGUI? _sellerText;
         [SerializeField] private TextMeshProUGUI? _descriptionText;
         [SerializeField] private Image? _iconImage;
 
+        private ItemSO? _itemSo;
         public void SetItem(ItemSO itemSo)
         {
+            _itemSo = itemSo;
             if (_iconImage != null)
             {
                 _iconImage.sprite = itemSo.ItemIcon;
             }
+        }
 
-            // TODO: Spam typing bit
+        public void Clear()
+        {
+            if (_titleText != null)
+            {
+                _titleText.text = "";
+            }
+            if (_sellerText != null)
+            {
+                _sellerText.text = "";
+            }
+            if (_descriptionText != null)
+            {
+                _descriptionText.text = "";
+            }
+        }
+
+        public int GetTotalTextLength()
+        {
+            if (_itemSo == null)
+                return 0;
+
+            return _itemSo.ItemName.Length + SellerName.Length + _itemSo.ItemDescription.Length;
         }
     }
 }
