cc8d92a4c1f34ad79b968800ca40f57b1656b779
legoandmars <legoandmars@gmail.com>
2024-04-13 12:33:08 -0700

    Item SO

---
+++ Assets/Runtime/Computers/ComputerItemEntryController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SoldByWizards.Items;
+using UnityEngine;
+
+namespace SoldByWizards.Computers
+{
+    public class ComputerItemEntryController : MonoBehaviour
+    {
+        // TODO: Hook up dynamically
+        [SerializeField] public List<ItemSO> CurrentItems = new();
+    }
+}
--- Assets/Runtime/Items/Item.cs
+++ Assets/Runtime/Items/Item.cs
@@ -6,13 +6,6 @@ namespace SoldByWizards.Items
 {
     public class Item : MonoBehaviour
     {
-        [SerializeField, Header("Basic Item Stats")] private string _itemName = "New Item";
-        [SerializeField] private float _baseSaleValue = 0.0f;
-
-        [SerializeField, Header("Stock Market")] private float _stockMarketLowModifier = 0.3f;
-        [SerializeField] private float _stockMarketHighModifier = 3.0f;
-        [SerializeField] private float _multiSalesPenalty = -0.03f;
-
-        [SerializeField] private bool _twoHanded = false;
+        public ItemSO? ItemSO;
     }
 }
