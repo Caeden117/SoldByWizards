927cb6715ae2a64df54fa52249af322950a5fe0d
legoandmars <legoandmars@gmail.com>
2024-04-14 14:54:22 -0700

    pre shiping container color thing

---
+++ Assets/Runtime/Maps/ShippingContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoldByWizards.Maps
+{
+    // used for editor hackery to get random materials (lightmap baking no worky)
+    public class ShippingContainer : MonoBehaviour
+    {
+        public List<Renderer> ColoredRenderers = new();
+    }
+}
