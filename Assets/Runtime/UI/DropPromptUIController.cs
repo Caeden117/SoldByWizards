abfa571947c03d1e22edf36e99f2eba9c0bbaa44
Rabbit <58245404+PlusOneRabbit@users.noreply.github.com>
2024-04-15 16:03:16 -0700

    Add drop prompt

--- Assets/Runtime/UI/DropPromptUIController.cs
+++ Assets/Runtime/UI/DropPromptUIController.cs
@@ -1,40 +1,49 @@
 ﻿using AuraTween;
+using SoldByWizards.Items;
 using SoldByWizards.Player.Interactions;
 using UnityEngine;
 
 namespace SoldByWizards
 {
-    public class InteractionPromptUIController : MonoBehaviour
+    public class DropPromptUIController : MonoBehaviour
     {
         private const float _duration = 0.05f;
 
-        [SerializeField] private InteractionsManager _interactionsManager;
+        [SerializeField] private ItemsManager _itemsManager;
         [SerializeField] private CanvasGroup _canvasGroup;
         [SerializeField] private CanvasGroup _crosshairCanvasGroup;
         [SerializeField] private TweenManager _tweenManager;
 
+        private bool _hasSeenPrompt = false;
+
         private void Start()
         {
-            _interactionsManager.OnInteractionHover += OnInteractionHover;
-            _interactionsManager.OnInteractionUnhover += OnInteractionUnhover;
+            _itemsManager.OnItemPickup += OnItemPickup;
+            _itemsManager.OnItemDrop += OnItemDrop;
         }
 
-        private void OnInteractionHover()
+        private void OnItemPickup(int _, Item __)
         {
+            if (_hasSeenPrompt) return;
+
             _tweenManager.Run(0f, 1f, _duration, a => _canvasGroup.alpha = a, Easer.InOutSine);
             _tweenManager.Run(1f, 0f, _duration, a => _crosshairCanvasGroup.alpha = a, Easer.InOutSine);
         }
 
-        private void OnInteractionUnhover()
+        private void OnItemDrop(int _, Item __)
         {
+            if (_hasSeenPrompt) return;
+
             _tweenManager.Run(1f, 0f, _duration, a => _canvasGroup.alpha = a, Easer.InOutSine);
             _tweenManager.Run(0f, 1f, _duration, a => _crosshairCanvasGroup.alpha = a, Easer.InOutSine);
+
+            _hasSeenPrompt = true;
         }
 
         private void OnDestroy()
         {
-            _interactionsManager.OnInteractionHover -= OnInteractionHover;
-            _interactionsManager.OnInteractionUnhover -= OnInteractionUnhover;
+            _itemsManager.OnItemPickup -= OnItemPickup;
+            _itemsManager.OnItemDrop -= OnItemDrop;
         }
     }
 }
