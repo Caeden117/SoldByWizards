4a0ca8fefc4916bd02f1c787257fa799324c414b
legoandmars <legoandmars@gmail.com>
2024-04-15 14:10:02 -0700

    icon tech

---
+++ Assets/Runtime/Editor/ItemCameraData.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using SoldByWizards.Items;
+using UnityEngine;
+
+namespace SoldByWizards.Editor
+{
+    [Serializable]
+    public class ItemCameraData
+    {
+        public Item Item;
+        public float Height;
+        public float Length;
+        public float CameraRotation;
+    }
+}
+#endif
